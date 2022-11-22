99666a027a8be0b44aa0177a36eaa810efffaed1
Marcos Flavio <mfghost69@gmail.com>
2022-11-22 01:20:31 -0300

    Adding messages

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use std::io;
+
+fn main() {
+    let mut _counter: u64 = 0;
+    let mut input = String::new();
+
+    println!("Type a number for the test: ");
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    let mut input: u64 = input.trim().parse().expect("Please pass a number");
+
+    println!("Number chosen: {}", input);
+
+    while input > 1 {
+        if input % 2 == 0 {
+            println!("Even. Dividing by 2...");
+            input = input / 2;
+            println!("{}", input);
+            _counter += 1;
+        } else {
+            println!("Odd. Multiplying by 3 + 1...");
+            input = input * 3 + 1;
+            println!("{}", input);
+            _counter += 1;
+        }
+    }
+    println!("Total of tests made: {}", _counter);
+}
